f75e8a31709c8c9a20d96ad80ba4c8cf8b2a05f6
DeAndre Johnson <dre@phreshr.com>
2019-10-21 02:10:27 +0800

    update theme

--- frontend/src/style/theme.js
+++ frontend/src/style/theme.js
@@ -1,3 +1,5 @@
+import { rem, tint } from 'polished';
+
 const gradients = {
   purplePink: 'linear-gradient(91.78deg, #6D58C6 -9.27%, #F977A1 90.73%)',
   purple: 'linear-gradient(137.08deg, #230A11 -1.96%, #5D0D5F 97.8%)',
@@ -6,32 +8,44 @@ const gradients = {
 const colors = {
   white: 'white',
   black: '#111',
-  darkPurple: '#3F0B37',
-  gradients,
+  purple: '#380B2D',
+  lightPurple: tint(0.5, '#380B2D'),
 };
 
 export default {
   colors: {
     ...colors,
-    bgColor: colors.white,
-    textColor: colors.black,
-    sideBarColor: colors.darkPurple,
-    buttonColor: colors.gradients.purplePink,
+    baseFont: colors.black,
+    headingFont: colors.lightPurple,
+    h1Font: colors.purple,
   },
-  font: {
-    body: '"Poppins", sans-serif',
-    heading: '"Poppins", sans-serif',
-    spacing: {
-      xs: '4px',
-      sm: '8px',
-      md: '16px',
-      lg: '24px',
-      xl: '32px',
+  gradients,
+  fontFamily: {
+    base: "'Poppins', sans-serif",
+    heading: "'Poppins', sans-serif",
+  },
+  typography: {
+    fontSize: '1rem',
+    fontWeight: {
+      light: '300',
+      regular: '400',
+      bold: '500',
     },
-    breakpoints: {
-      sm: '576px',
-      md: '768px',
-      lg: '992px',
-    },
+    lineHeight: '1.6',
+  },
+  backgrounds: {
+    sideNav: gradients.purple,
+    buttons: gradients.purplePink,
+  },
+  spacing: {
+    sm: rem(4),
+    md: rem(8),
+    lg: rem(16),
+    xl: rem(32),
+  },
+  breakpoints: {
+    sm: 576, // phone
+    md: 768, // tablet
+    lg: 992, // desktop
   },
 };
