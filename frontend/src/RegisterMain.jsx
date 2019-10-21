dd93b99a5b67dc38a982cd3997820ac341f0ccbd
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-10-21 13:06:13 -0500

    set up mapPropsToValues and validationSchema

--- frontend/src/RegisterMain.jsx
+++ frontend/src/RegisterMain.jsx
@@ -4,30 +4,44 @@ import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
 
-const RegisterMain = () => {
+const RegisterMain = ({ values, errors, touched }) => {
     return (
         <div className='register-main'>
             <Form>
-                <Field type='text' name='email' placeholder='Email' />
+                <Field type='text' name='email' placeholder='Email' value={values.email}/>
                 <Field type='text' name='username' placeholder='Username' />
-                <Field type='text' name='password' placeholder='password' />
+                <Field type='text' name='password' placeholder='Password' />
                 <Field type='text' name='confirm' placeholder='Confirm Password' />
                 <label className='checkbox-container'>
                 I agree to all terms and conditions
                 <Field
                     type='checkbox'
                     name='terms'
+                    value={values.terms}
                 />
                 </label>
-                <button className='register-button'>Register</button>
+                <button type='submit' className='register-button'>Register</button>
             </Form>
         </div>
     )
-    
 }
 
 const FormikRegisterMain = withFormik({
-
+    mapPropsToValues({ email, username, password, confirm, terms }) {
+        return {
+            email: email || '',
+            username: username || '',
+            password: password || '',
+            confirm: confirm || '',
+            terms: terms || false // Currently unable to check and uncheck box
+        }
+    },
+    validationSchema: Yup.object().shape({
+        email: Yup.string().reqiured,
+        username: Yup.string().reqiured,
+        password: Yup.string().reqiured,
+        confirm: Yup.string().reqiured,
+    })
 })(RegisterMain)
 
 export default FormikRegisterMain;
