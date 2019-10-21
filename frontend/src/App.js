6930c7f846cbaf9f05fe19df43c59282a63edf90
Emily_Richard <richard.emily.e@gmail.com>
2019-10-21 19:49:36 -0400

    added header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import styled from 'styled-components/macro';
 import media from './style/mq';
 
 import { useAuth } from './context/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+
+import LoginForm from './components/LoginForm';
+import CreateForm from './components/CreateForm';
 
 const AppContainer = styled.div`
   font-family: ${props => props.theme.fontFamily.base};
@@ -22,9 +26,9 @@ function App() {
         <Route exact path="/">
           {!state.isAuthenticated ? <Redirect to="/login" /> : <h1>Home</h1>}
         </Route>
-        <Route exact path="/login">
-          <h1>Login</h1>
-        </Route>
+        <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/create" component={CreateForm} />
+        <Route exact path="/update/:id" /*component={UpdateForm}*/ />
       </Switch>
     </AppContainer>
   );
