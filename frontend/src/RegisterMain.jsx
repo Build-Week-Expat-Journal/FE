09fbaac4e1a5e46498a8aeefc5f7bd5e0bbb495d
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-10-21 14:49:09 -0500

    Validation and post working

--- frontend/src/RegisterMain.jsx
+++ frontend/src/RegisterMain.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
@@ -9,8 +9,13 @@ const RegisterMain = ({ values, errors, touched }) => {
         <div className='register-main'>
             <Form>
                 <Field type='text' name='email' placeholder='Email' value={values.email}/>
+                    {touched.email && errors.email ? (
+                        <p>{errors.email}</p>
+                    ) : undefined}
                 <Field type='text' name='username' placeholder='Username' />
+                    {touched.username && errors.username && (<p>{errors.username}</p>)}
                 <Field type='text' name='password' placeholder='Password' />
+                    {touched.password && errors.password && (<p>{errors.password}</p>)}
                 <Field type='text' name='confirm' placeholder='Confirm Password' />
                 <label className='checkbox-container'>
                 I agree to all terms and conditions
@@ -37,11 +42,25 @@ const FormikRegisterMain = withFormik({
         }
     },
     validationSchema: Yup.object().shape({
-        email: Yup.string().reqiured,
-        username: Yup.string().reqiured,
-        password: Yup.string().reqiured,
-        confirm: Yup.string().reqiured,
-    })
+        email: Yup.string()
+            .email('Invalid email')
+            .required('Required'),
+        username: Yup.string()
+            .min(2, 'Too short - at least 3 characters!')
+            .max(20, 'Too Long - no more than 20 characters!')
+            .required('Required'),
+        password: Yup.string()
+            .min(6, 'Too short - at least 6 characters!')
+            .max(20, 'Too Long - no more than 20 characters!')
+            .required('Required'),
+        confirm: Yup.string(),
+    }),
+    handleSubmit(values, {setStatus}) {
+        axios
+            .post('https://reqres.in/api/users/', values)
+            .then(res => { console.log(res.data) })
+            .catch(err => console.log( err.response ))
+    }
 })(RegisterMain)
 
 export default FormikRegisterMain;
