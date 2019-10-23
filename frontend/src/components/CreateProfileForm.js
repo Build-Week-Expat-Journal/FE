06034625728b06e667a54210f47a3dace56c5715
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-10-22 19:19:56 -0500

    Fixed bugs in CPF; posting to backend api

--- frontend/src/components/CreateProfileForm.js
+++ frontend/src/components/CreateProfileForm.js
@@ -4,69 +4,72 @@ import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import styled from 'styled-components';
 
-const CreateProfileForm = ({ values, errors, touched }) => {
-
-    // Styled Components 
+// Styled Components 
     
 
-    const StyledForm = styled(Form)`
-    display: flex;
-    flex-direction: column;
-    width: 80%;
-    `
+const StyledForm = styled(Form)`
+display: flex;
+flex-direction: column;
+width: 80%;
+`
 
 
 
-    const StyledField = styled(Field)`
-    color: white;
-    font-size: 14px;
-    background: transparent;
-    padding: 4%;
-    border-radius: 10px;
-    margin-bottom: 2%;
-    `
+const StyledField = styled(Field)`
+color: white;
+font-size: 14px;
+background: transparent;
+padding: 4%;
+border-radius: 10px;
+margin-bottom: 2%;
+`
 
 
-    const FormButton = styled.button`
-    position: relative;
-    background-image: linear-gradient(
-        90deg,
-        rgba(109, 88, 198, 1) 0%,
-        rgba(249, 119, 161, 1) 100%
-    );
+const FormButton = styled.button`
+position: relative;
+background-image: linear-gradient(
+    90deg,
+    rgba(109, 88, 198, 1) 0%,
+    rgba(249, 119, 161, 1) 100%
+);
+border-radius: 12px;
+box-sizing: border-box;
+color: #111;
+display: block;
+height: 48px; /* make theme variable: inputHeight? */
+letter-spacing: 1px;
+margin: calc(${props => props.theme.spacing.md} * 2) auto;
+padding: 2px;
+border: none;
+text-decoration: none;
+text-transform: uppercase;
+width: 100%;
+z-index: 2;
+span {
+    align-items: center;
+    background: #e5e5e5;
     border-radius: 12px;
-    box-sizing: border-box;
-    color: #111;
-    display: block;
-    height: 48px; /* make theme variable: inputHeight? */
-    letter-spacing: 1px;
-    margin: calc(${props => props.theme.spacing.md} * 2) auto;
-    padding: 2px;
-    border: none;
-    text-decoration: none;
-    text-transform: uppercase;
+    display: flex;
+    justify-content: center;
+    height: 100%;
+    transition: background 0.5s ease;
     width: 100%;
-    z-index: 2;
-    span {
-        align-items: center;
-        background: #e5e5e5;
-        border-radius: 12px;
-        display: flex;
-        justify-content: center;
-        height: 100%;
-        transition: background 0.5s ease;
-        width: 100%;
-    }
-    &:hover {
-        cursor: pointer;
-    }
-    `
+}
+&:hover {
+    cursor: pointer;
+}
+`
 
-    const ErrorMessage = styled.div`
-        font-family: inherit;
-        color: #e5195f;
-        font-size: 12px;
-    `
+const ErrorMessage = styled.div`
+    font-family: inherit;
+    color: #e5195f;
+    font-size: 12px;
+`
+
+
+const CreateProfileForm = ({ values, errors, touched }) => {
+
+    
 
 
     return (
@@ -118,7 +121,7 @@ const FormikCreateProfileForm = withFormik({
         bio: Yup.string()
             .max(200, 'Too Long - no more than 200 characters!')
     }),
-    handleSubmit(values, {setStatus}) {
+    handleSubmit(values) {
         axios
             .post('https://reqres.in/api/users/', values)
             .then(res => { console.log(res.data) })
