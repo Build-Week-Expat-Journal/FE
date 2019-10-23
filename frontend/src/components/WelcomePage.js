c0ab6110ceaa9e7fc61b658c9a098e6424dadef7
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-10-23 14:57:01 -0500

    created temporary mockProfileData

--- frontend/src/components/WelcomePage.js
+++ frontend/src/components/WelcomePage.js
@@ -1,8 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
 const WelcomePage = () => {
+    const mockProfileData=[
+        {
+            id: 1,
+            first_name: 'Marty',
+            last_name: 'Smith',
+            date_of_birth: '3/4/1992',
+            occupation: 'React Developer',
+            location: 'CA',
+            bio: 'React Developer from Cali'
+        }
+    ]
+
+    const [profileData, setProfileData] = useState([])
+
+    useEffect(() => {
+        axios
+            .get(`https://expat-journal-lambda1.herokuapp.com/api/user_profile`)
+            .then(response => console.log(response))
+    }, [])
+
     return (
         <div></div>
     )
