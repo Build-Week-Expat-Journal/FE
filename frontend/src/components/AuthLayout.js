4abe28c8f114bd814cbc32cdffc8b2a7334582cc
DeAndre Johnson <dre@phreshr.com>
2019-10-25 01:24:59 +0800

    refactored auth layout

--- frontend/src/components/AuthLayout.js
+++ frontend/src/components/AuthLayout.js
@@ -10,7 +10,7 @@ const Wrapper = styled.div`
   grid-template-areas: ${props =>
     props.formAlign === 'right' ? "'intro form'" : "'form intro'"};
   grid-template-columns: ${props =>
-    props.formAlign === 'right' ? '1fr 40%' : '40% 1fr'};
+    props.formAlign === 'right' ? '1fr 400px' : '400px 1fr'};
   grid-column-gap: 0px;
   grid-row-gap: 20px;
   justify-items: stretch;
@@ -59,7 +59,7 @@ const Intro = styled.div`
 
   h1,
   h2 {
-    font-size: ${rem(80)};
+    font-size: ${rem(64)};
     font-weight: ${props => props.theme.typography.fontWeight.regular};
     color: white;
     z-index: 2;
@@ -82,6 +82,58 @@ const Intro = styled.div`
   }
 `;
 
+const IntroContent = styled.section`
+  display: grid;
+  grid-template-areas:
+    'header'
+    'middle'
+    'bottom';
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(3, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+  padding: 0 4rem;
+  z-index: 1;
+
+  header {
+    grid-area: header;
+    h1 {
+      font-size: ${props => (props.titleSize ? rem(props.titleSize) : rem(64))};
+      font-weight: ${props => props.theme.typography.fontWeight.regular};
+      color: white;
+      text-align: ${props => (props.titleSize < 64 ? 'center' : 'left')};
+      z-index: 2;
+    }
+  }
+  .middle {
+    grid-area: middle;
+    h2 {
+      font-size: ${rem(64)};
+      font-weight: ${props => props.theme.typography.fontWeight.regular};
+      color: white;
+      text-align: center;
+      z-index: 2;
+      span {
+        background: ${props => props.theme.gradients.purplePink};
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+      }
+    }
+  }
+  .bottom {
+    grid-area: bottom;
+    display: flex;
+    width: 80%;
+    padding: 0 2rem;
+    z-index: 2;
+
+    p {
+      font-weight: 500;
+      font-size: 1.25rem;
+    }
+  }
+`;
+
 const Form = styled.div`
   grid-area: form;
   display: flex;
@@ -99,19 +151,28 @@ const Form = styled.div`
   }
 `;
 
-const AuthLayout = ({ formAlign = 'right', message, children }) => {
+const AuthLayout = ({
+  formAlign = 'right',
+  titleSize,
+  subTitle,
+  message,
+  children,
+}) => {
   return (
     <Wrapper formAlign={formAlign}>
       <Intro>
         <div className="filter"></div>
-        {message ? (
-          <div className="message">{message()}</div>
-        ) : (
-          <h1>
-            Expat <br />
-            Journel
-          </h1>
-        )}
+        <IntroContent titleSize={titleSize}>
+          <header>
+            <h1>Expat Journel</h1>
+          </header>
+          <section className="middle">
+            {subTitle ? <h2>{subTitle()}</h2> : null}
+          </section>
+          <section className="bottom">
+            {message ? <p>{message()}</p> : null}
+          </section>
+        </IntroContent>
       </Intro>
       <Form>{children}</Form>
     </Wrapper>
